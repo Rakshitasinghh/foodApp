b87027e53c4d8de87a5ca17d0330f1ee82d3dda9
Rakshitasinghh <rakshita.singhhh@gmail.com>
2025-02-26 21:26:33 +0530

    updated details

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,7 +6,7 @@ export const About = () => {
             <div className="p-24 grid grid-cols-2">
                 <div>
                     <h2 className="text-2xl font-medium">About us</h2>
-                    <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus omnis ipsum corporis dolorem nihil in porro perspiciatis a neque aliquam. Vitae porro hic, odio delectus labore unde debitis enim asperiores?</p>
+                    <p className="text-lg">Welcome to our website, your go-to food delivery app for delicious meals anytime, anywhere. We bring you a wide range of cuisines from top restaurants with fast and hassle-free delivery. Enjoy easy ordering, exclusive deals, and a seamless food experience. Satisfy your cravings with just a few taps!</p>
                 </div>
                 <div className="flex items-center justify-center">
                     <img src={aboutImage} alt="About" className="w-[400px] h-[400px] object-cover" />
@@ -14,6 +14,6 @@ export const About = () => {
             </div>
         </div>
     );
-};
+};// system setup
 
 export default About;
